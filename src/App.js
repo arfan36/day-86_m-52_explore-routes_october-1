bfed20a57db00c853c4ddb34ca0e62ad44d451de
arfan36 <arfanur36@gmail.com>
2022-10-05 21:43:45 +0600

    52-3 (interesting) Nested route and outlet for shared content

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import Products from './components/Products/Products';
+import Main from './components/layout/Main';
+import Friends from './components/Friends/Friends';
 
 function App() {
   const router = createBrowserRouter([
-    { path: '/', element: <div>This is default page</div> },
-    { path: '/home', element: <Home></Home> },
-    { path: '/about', element: <About></About> },
-    {path: '/products', element: <Products></Products>}
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        { path: '/', element: <Home></Home> },
+        { path: '/home', element: <Home></Home> },
+        { path: '/products', element: <Products></Products> },
+        { path: '/friends', element: <Friends></Friends> }
+      ],
+    },
+    { path: '/about', element: <About></About> }
   ]);
   return (
     <div className="App">
