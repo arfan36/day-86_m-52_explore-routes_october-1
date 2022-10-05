5947e059a8b82e09ba726558fbd30d0980b4eb4c
arfan36 <arfanur36@gmail.com>
2022-10-05 19:31:31 +0600

    52-2 Create Simple Route with Links and Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <div>This is default page</div> },
-    { path: '/home', element: <div>Home Page</div> },
-    { path: '/about', element: <div>This is About Page</div> }
+    { path: '/home', element: <Home></Home> },
+    { path: '/about', element: <About></About> },
+    {path: '/products', element: <Products></Products>}
   ]);
   return (
     <div className="App">
--- src/__steps.js
+++ src/__steps.js
@@ -4,3 +4,10 @@
 3. Add RouterProvider and pass router props
 4. Create some route
  */
+
+/* 
+//@ ───────────────────────────────────── THINGS YOU NEED TO CREATE A ROUTE ─────
+1. create a Link: so that you can go to this route
+2. Create Component: to add what you will show once you go to that route
+3. Add route so that react router know the Component it needs to display while you are visiting to that route 
+ */
